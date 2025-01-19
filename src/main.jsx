b617805b0bc2b6ea14d2d2e6b591a6890afc8f7c
Rajibbepary <rajib635356@gmail.com>
2025-01-19 14:49:54 +0600

    tanstack provider setup

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,24 @@ import { router } from './routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider';
 import { ToastContainer } from 'react-toastify';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
     <ToastContainer />
+    <QueryClientProvider client={queryClient}>
     <HelmetProvider>
     <div className='max-w-screen-xl mx-auto'>
      <RouterProvider router={router} />
      </div>
     </HelmetProvider>
+    </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
